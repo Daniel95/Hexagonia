2b44abfec1100db76357b1b61447e47d7ad51391
Daniel95 <idaniel95@me.com>
2018-04-13 13:36:51 +0200

    added new player scene with default cardboard scripts

---
+++ Assets/Scripts/Player/MovementPlane.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlane : MonoBehaviour {
+
+    [SerializeField] private Transform hmdTransform;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void CalculatePlanePosition()
+    {
+        //RaycastHit
+
+        //hmdTransform.forward
+    }
+
+}
