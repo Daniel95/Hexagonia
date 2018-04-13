73351ea039de1fb405d2f122ad1c1e1066fca37b
DANNYPC\danny <dannykruiswijk@hotmail.com>
2018-04-13 10:46:30 +0200

    Added enum CoinType

---
+++ Assets/Scripts/Chunk/CoinType.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// CoinType is an enum with 5 types: Common, Uncommon, Rare,
+/// It is used in multiple classes. One of them is the RedirectAbility class.
+/// </summary>
+
+public enum CoinType {
+    Common,
+    Uncommon,
+    Rare,
+    Epic,
+    Legendary
+}
