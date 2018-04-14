94459778786fc81fbcaeebe70d4c05f07f4b2f85
Daniel Brand <idaniel95@me.com>
2018-04-14 21:20:21 +0200

    added delay to playermovement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,31 +1,20 @@
-﻿using DG.Tweening;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    [SerializeField] private float maxSpeed = 0.3f;
+    [SerializeField] [Range(0, 1)] private float speed = 0.3f;
 
     private void UpdateTargetPosition(Vector3 _targetPosition)
     {
         if (transform.position == _targetPosition)
             return;
 
-        transform.position = _targetPosition;
-
-
-
-        //Vector3 offset = transform.position - pointOnPlane;
-        //Vector3 direction = offset.normalized;
-        /*
         Vector3 offset = _targetPosition - transform.position;
+        Vector3 direction = offset.normalized;
+        float distance = Vector3.Distance(_targetPosition, transform.position);
 
-        Vector3 move = VectorHelper.Clamp(offset, -maxSpeed, maxSpeed);
-
-        transform.Translate(move);
-
-        //transform.DOMove(_targetPosition, 90).SetEase(Ease.InElastic);
-        */
+        transform.position += direction * (distance * speed);
     }
 
     private void OnEnable()
