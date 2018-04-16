7f616a35184facc912798df83e61735a89537957
DANNYPC\danny <dannykruiswijk@hotmail.com>
2018-04-16 14:16:19 +0200

    Setup basic structure

---
+++ Assets/Scripts/Chunk/ChunkMover.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ChunkMover is a script responsible for moving all the spawned chunks in the level
+/// It contains a speed variable which is influenced by the progress of the game
+/// </summary>
+
+public class ChunkMover : MonoBehaviour
+{
+    public static Action<Chunk> ChunkRemovedAction;
+
+    [SerializeField] private float speed;
+
+    [SerializeField] private float despawnZPosition;
+
+    private List<Chunk> currentChunks = new List<Chunk>();
+	
+	void Update () {
+	    for (int i = 0; i < currentChunks.Count; i++)
+	    {
+	        if (currentChunks[i].transform.position.z < despawnZPosition)
+	        {
+	            RemoveChunk();
+	        }
+            currentChunks[i].transform.Translate(Vector3.back * speed * Time.deltaTime);
+
+	    }
+	}
+
+    private void AddChunk(Chunk _chunk)
+    {
+        currentChunks.Add(_chunk);
+    }
+
+    private void RemoveChunk()
+    {
+        Chunk _removedChunk = currentChunks[0];
+        currentChunks.Remove(_removedChunk);
+        ChunkRemovedAction(_removedChunk);
+    }
+
+    private void OnEnable()
+    {
+        ChunkPool.ChunkSpawnedAction += AddChunk;
+    }
+
+    private void OnDisable()
+    {
+        ChunkPool.ChunkSpawnedAction -= AddChunk;
+    }
+}
---
+++ Assets/Scripts/Chunk/ChunkPool.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+/// <summary>
+/// The chunkpool has a list with all the available chunk prefabs
+/// This script is responsible for Initialising the next chunk when asked
+/// </summary>
+
+public class ChunkPool : MonoBehaviour
+{
+    public static Action<Chunk> ChunkSpawnedAction;
+
+    [SerializeField] private int amountOfChunks;
+    [SerializeField] private float chunkStartPosition;
+
+    [SerializeField] private List<Chunk> availableChunkPrefabs;
+
+    private float totalChunkLengths;
+
+    void Start()
+    {
+        for (int i = 0; i < amountOfChunks; i++)
+        {
+            SpawnChunk();
+        }
+    }
+
+    private void OnRemovedChunk(Chunk _chunk)
+    {
+        totalChunkLengths -= _chunk.Length;
+        SpawnChunk();
+    }
+
+    private void SpawnChunk()
+    {
+        Vector3 _spawnPosition = new Vector3(0, 0, (chunkStartPosition + totalChunkLengths));
+
+        GameObject _spawnedChunkGameObject = Instantiate(GenerateRandomChunk().gameObject, _spawnPosition, Quaternion.identity);
+        Chunk _spawnedChunk = _spawnedChunkGameObject.GetComponent<Chunk>();
+
+        totalChunkLengths += _spawnedChunk.Length;
+
+        ChunkSpawnedAction(_spawnedChunk);
+
+    }
+
+    private Chunk GenerateRandomChunk()
+    {
+        int _randomNumber = Random.Range(0, availableChunkPrefabs.Count);
+        return availableChunkPrefabs[_randomNumber];
+    }
+
+    private void OnEnable()
+    {
+        ChunkMover.ChunkRemovedAction += OnRemovedChunk;
+    }
+
+    private void OnDisable()
+    {
+        ChunkMover.ChunkRemovedAction -= OnRemovedChunk;
+    }
+}
