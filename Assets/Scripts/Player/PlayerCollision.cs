d7f9f35644c645e62e9ed68d8b4d55b7236354c4
DANNYPC\danny <dannykruiswijk@hotmail.com>
2018-04-24 16:41:40 +0200

    Edited all chunks for gameplay, added more coin positions, tweaked obstacles

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour {
 
-    private void OnTriggerEnter(Collision _other)
+    private void OnTriggerEnter(Collider _otherCollider)
     {
-
+        if (_otherCollider.tag == Tags.Obstacle)
+        {
+            Destroy(gameObject);
+            CoroutineHelper.Delay(60, () =>
+            {
+                SceneManager.LoadScene(0);
+            });
+        }
+        
     }
 }
