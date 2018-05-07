7213c8f9c6de6732f2c551226fd2d7dcc771edbe
Pim Horeman <pimhoreman@gmail.com>
2018-05-07 16:24:55 +0200

    Added ResourceBar Added ResourceBar Prefab Changed ResourceBarUI Script Duplicated Resourcebar png

--- Assets/Scripts/UI/ResourceBarUI.cs
+++ Assets/Scripts/UI/ResourceBarUI.cs
@@ -2,11 +2,12 @@
 
 public class ResourceBarUI : MonoBehaviour
 {
-
 	private float scaleX = 0f;
-	private float scaleBarByScore;
-	private float offset = 1f;
+	private float scaleBarByScore = 0f;
+	private float offset = -2.3f;
 	[SerializeField] private GameObject resourceBar;
+
+	private float localScaleX;
 
 	private void Awake()
 	{
@@ -20,11 +21,11 @@ public class ResourceBarUI : MonoBehaviour
 
 	private void UpdateResourceBar()
 	{
+
 		if (scaleBarByScore < 1)
 		{
 			scaleBarByScore = scaleX + ResourceValue.Instance.Value;
-			resourceBar.transform.position = new Vector3((scaleBarByScore - offset) / 0.5f, resourceBar.transform.position.y, resourceBar.transform.position.z);
-
+			resourceBar.transform.position = new Vector3(offset, resourceBar.transform.position.y, resourceBar.transform.position.z);
 			resourceBar.transform.localScale = new Vector3(scaleBarByScore, resourceBar.transform.localScale.y, resourceBar.transform.localScale.z);
 		}
 	}
