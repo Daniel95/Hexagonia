06c66fc6e222dbf9e472177b414a4d15d1914d50
Pim Horeman <pimhoreman@gmail.com>
2018-05-08 16:18:57 +0200

    -Updated ResourceBarUI.cs (It now decreases smoothly)

--- Assets/Scripts/UI/ResourceBarUI.cs
+++ Assets/Scripts/UI/ResourceBarUI.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class ResourceBarUI : MonoBehaviour
 {
 	private float scaleX = 0f;
 	private float scaleBarByScore = 0f;
 	private float offset = -2.3f;
+	private float waitTime = 4f;
 	[SerializeField] private GameObject resourceBar;
 
 	private void Awake()
@@ -22,14 +24,22 @@ public class ResourceBarUI : MonoBehaviour
 		if (scaleBarByScore < 1)
 		{
 			scaleBarByScore = scaleX + ResourceValue.Instance.Value; //Chained with the coin pick-up
-			resourceBar.transform.position = new Vector3(offset, resourceBar.transform.position.y, resourceBar.transform.position.z);
-			resourceBar.transform.localScale = new Vector3(scaleBarByScore, resourceBar.transform.localScale.y, resourceBar.transform.localScale.z);
-
-			scaleBarByScore -= 0.2f * Time.deltaTime;
 		}
-		if(scaleBarByScore == 1)
+		if(scaleBarByScore != 0)
 		{
-			
+			StartCoroutine("DecreaseOverTime");
 		}
+		if(scaleBarByScore > 1) //BUG, Bar not decreasing when over 1 / when full
+		{
+			scaleX = 1f;
+		}
+		resourceBar.transform.position = new Vector3(offset, resourceBar.transform.position.y, resourceBar.transform.position.z);
+		resourceBar.transform.localScale = new Vector3(scaleBarByScore, resourceBar.transform.localScale.y, resourceBar.transform.localScale.z);
+	}
+
+	private IEnumerator DecreaseOverTime()
+	{
+		scaleX -= 0.05f * Time.deltaTime;
+		yield return new WaitForSeconds(waitTime);
 	}
 }
