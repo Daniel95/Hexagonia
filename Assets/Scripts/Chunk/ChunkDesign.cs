798b6a3c6d5bc1845230bb7f4ccb4c544df089c0
Daniel Brand <idaniel95@me.com>
2018-05-09 21:19:46 +0200

    ignore editor code for platforms other that the unity editor in chunkdesign script

--- Assets/Scripts/Chunk/ChunkDesign.cs
+++ Assets/Scripts/Chunk/ChunkDesign.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
 /// <summary>
