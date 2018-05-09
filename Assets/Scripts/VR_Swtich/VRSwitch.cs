b16a02951d9519181e9a881854f0abbc395e36fd
tommtom9 <tommtom9@live.nl>
2018-05-09 09:48:46 +0200

    - trying out multipl ways of working with the gyrocamera

---
+++ Assets/Scripts/VR_Swtich/VRSwitch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class VRSwitch : MonoBehaviour
+{
+    public void Switch()
+    {
+        XRSettings.enabled = !XRSettings.enabled;
+    }
+
+    private void Start()
+    {
+        Debug.Log("Switching");
+        XRSettings.enabled = false;
+    }
+}
