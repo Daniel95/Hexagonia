1ad68f962988eb0c30af421f96a453f06f7ffeb0
Daniel Brand <idaniel95@me.com>
2018-05-09 12:02:54 +0200

    added scene default scene that contains managers that are used throughout the game, this scene is never unloaded. also added scene loader that can load and unload scenes.

---
+++ Assets/Scripts/Scene/SceneLoader.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class SceneLoader : MonoBehaviour {
+
+    public static SceneLoader Instance { get { return GetInstance(); } }
+
+    #region Singleton
+    private static SceneLoader instance;
+
+    private static SceneLoader GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = FindObjectOfType<SceneLoader>();
+        }
+        return instance;
+    }
+    #endregion
+
+    [SerializeField] private Scenes startScene;
+
+    private Scenes? currentScene;
+
+    public void SwitchScene(Scenes newScene)
+    {
+        if (startScene == Scenes.Default)
+        {
+            Debug.LogWarning("Cannot switch to the Default scene.");
+            return;
+        }
+
+        Scenes? previousScene = currentScene;
+        currentScene = newScene;
+
+        if (previousScene != null)
+        {
+            SceneHelper.UnloadSceneOverTime(previousScene.ToString(), () => SceneHelper.LoadSceneOverTime(newScene.ToString()));
+        }
+        else
+        {
+            SceneHelper.LoadSceneOverTime(newScene.ToString());
+        }
+
+    }
+
+    private void Awake()
+    {
+        if(startScene == Scenes.Default)
+        {
+            Debug.LogWarning("Cannot load the Default scene as start scene.");
+            return;
+        }
+
+        currentScene = startScene;
+        SceneHelper.LoadSceneOverTime(startScene.ToString());
+    }
+
+}
