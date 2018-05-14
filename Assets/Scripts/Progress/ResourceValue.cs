6da875d4e5a0e71b2412977f8ebff810a8c64a61
Pim Horeman <pimhoreman@gmail.com>
2018-05-14 10:59:28 +0200

    Changed ResourceValue.cs Changed ResourceBarUI.cs

--- Assets/Scripts/Progress/ResourceValue.cs
+++ Assets/Scripts/Progress/ResourceValue.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System;
 
 public class ResourceValue : MonoBehaviour
 {
-
     public static ResourceValue Instance { get { return GetInstance(); } }
 
     #region Instance
@@ -24,12 +25,55 @@ public class ResourceValue : MonoBehaviour
 
     private float value;
 
-    private void OnScoreUpdatedEvent(int _score)
-    {
-        value += resouceIncreaseOnPickup;
-    }
+	public float maxValue = 1f; //Maximum Resource Value 
+	private float minValue = 0f; //Minumum Resource Value
+	private float waitTime = 0.5f;
 
-    private void OnEnable()
+	private Coroutine coroutineIncrease, coroutineDecrease;
+
+
+	private void Awake()
+	{
+		value = 0;
+	}
+
+	private void OnScoreUpdatedEvent(int _score)
+    {
+		coroutineIncrease = StartCoroutine(IncreaseOverTime());
+	}
+
+	private IEnumerator DecreaseOverTime()
+	{
+		value -= 0.1f;
+
+		ResourceBarUI.Instance.UpdateResourceBar();
+
+		yield return new WaitForSeconds(waitTime);
+
+		StopCoroutine(coroutineDecrease);
+	}
+
+	private IEnumerator IncreaseOverTime()
+	{
+		if (value > maxValue)
+		{
+			value = 1f;
+		}
+		else
+		{
+			value += resouceIncreaseOnPickup; //0 = 0 + 0.3 = 0.3  || 0.3 = 0.3 + 0.3 = 0.6 || 0.9 = 0.9 + 0.3 = 1.2
+			ResourceBarUI.Instance.UpdateResourceBar();
+
+		}
+
+		yield return new WaitForSeconds(waitTime);
+
+		coroutineDecrease = StartCoroutine(DecreaseOverTime());
+		StopCoroutine(coroutineIncrease);
+	}
+
+
+	private void OnEnable()
     {
         LevelProgess.ScoreUpdatedEvent += OnScoreUpdatedEvent;
     }
--- Assets/Scripts/UI/ResourceBarUI.cs
+++ Assets/Scripts/UI/ResourceBarUI.cs
@@ -4,87 +4,26 @@ using System;
 
 public class ResourceBarUI : MonoBehaviour
 {
-	private float maxValue = 1f; //Maximum Resource Value 
-	private float minValue = 0f; //Minumum Resource Value
-	private float currentValue; //Current Resource Value
 
-	private Coroutine coroutineIncrease, coroutineDecrease;
+	public static ResourceBarUI Instance { get { return GetInstance(); } }
 
+	#region Instance
+	private static ResourceBarUI instance;
 
-	private float waitTime = 0.5f;
+	private static ResourceBarUI GetInstance()
+	{
+		if (instance == null)
+		{
+			instance = FindObjectOfType<ResourceBarUI>();
+		}
+		return instance;
+	}
+	#endregion
+
 	[SerializeField] private GameObject resourceBar;
-
-	private void Awake()
+	
+	public void UpdateResourceBar()
 	{
-		resourceBar.transform.localScale = new Vector3(minValue, resourceBar.transform.localScale.y, resourceBar.transform.localScale.z);
-	}
-
-	private void UpdateResourceBar()
-	{
-		//-----------------------------
-		coroutineIncrease = StartCoroutine(IncreaseOverTime());
-		//-----------------------------
-
-		/*
-		currentValue = _score;
-
-		if (currentValue > minValue)
-		{
-			currentValue = Mathf.Clamp(currentValue - (0.1f * Time.deltaTime), minValue, maxValue);
-			//StartCoroutine("DecreaseOverTime");
-		}
-		if (currentValue <= minValue)
-		{
-			currentValue = 0;
-			Debug.Log("resourcebar is " + currentValue + " empty?");
-		}
-		resourceBar.transform.localScale = new Vector3(currentValue, resourceBar.transform.localScale.y, resourceBar.transform.localScale.z);
-		Debug.Log("Currentvalue is: " + currentValue);
-		*/
-	}
-
-	private IEnumerator DecreaseOverTime()
-	{
-		currentValue = currentValue - 0.1f;
-		Debug.Log("DecreaseOverTime CurrentValue: " + currentValue);
-		resourceBar.transform.localScale = new Vector3(Mathf.Clamp(currentValue - (0.01f * Time.deltaTime), minValue, maxValue), resourceBar.transform.localScale.y , resourceBar.transform.localScale.z);
-		//resourceBar.transform.localScale = new Vector3(currentValue, resourceBar.transform.localScale.y, resourceBar.transform.localScale.z);
-
-		yield return new WaitForSeconds(waitTime);
-
-		StopCoroutine(coroutineDecrease);
-	}
-
-	private IEnumerator IncreaseOverTime()
-	{
-		if(currentValue >= maxValue)
-		{
-			Debug.Log("BAR IS FULL");
-			currentValue = 1f;
-		}
-		else
-		{
-			currentValue = ResourceValue.Instance.Value;
-		}
-		Debug.Log("increaseOverTime CurrentValue: " + currentValue);
-
-		resourceBar.transform.localScale = new Vector3(Mathf.Clamp(currentValue, minValue, maxValue), resourceBar.transform.localScale.y, resourceBar.transform.localScale.z);
-
-		//resourceBar.transform.localScale = new Vector3(currentValue, resourceBar.transform.localScale.y, resourceBar.transform.localScale.z);
-
-		yield return new WaitForSeconds(waitTime);
-
-		coroutineDecrease = StartCoroutine(DecreaseOverTime());
-		StopCoroutine(coroutineIncrease);
-	}
-
-	private void OnEnable()
-	{
-		Coin.CoinCollectedEvent += UpdateResourceBar;
-	}
-
-	private void OnDisable()
-	{
-		Coin.CoinCollectedEvent -= UpdateResourceBar;
+		resourceBar.transform.localScale = new Vector3(Mathf.Lerp(ResourceValue.Instance.Value, ResourceValue.Instance.maxValue, 2f * Time.deltaTime), resourceBar.transform.localScale.y, resourceBar.transform.localScale.z);
 	}
 }
