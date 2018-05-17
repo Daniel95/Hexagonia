06486cf043b7b87d3908cedb3f7144e4a3b6d7d5
Daniel Brand <idaniel95@me.com>
2018-05-17 13:17:00 +0200

    added trail effect compatible with player

---
+++ Assets/Scripts/Player/Trail/PlayerTrail.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class PlayerTrail : MonoBehaviour
+{
+
+    [SerializeField] private int length;
+    [SerializeField] private float speed;
+
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = length;
+
+        for (int i = 0; i < length; i++)
+        {
+            float zPosition = transform.position.z - (i * speed);
+            lineRenderer.SetPosition(i, new Vector3(0, 0, zPosition));
+        }
+    }
+	
+	private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+
+        for (int i = length - 1; i > 0; i--)
+        {
+            Vector3 _previousPosition = lineRenderer.GetPosition(i - 1);
+
+            Vector3 _position = new Vector3(_previousPosition.x, _previousPosition.y, transform.position.z - (i * speed));
+            lineRenderer.SetPosition(i, _position);
+        }
+	}
+
+}
