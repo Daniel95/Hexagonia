3f9b0e7c84ce91c28c10998486aa1a985586d175
Daniel Brand <idaniel95@me.com>
2018-05-19 01:55:50 +0200

    removed obsolete animation code

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -23,8 +23,6 @@ public class Player : MonoBehaviour
 
     [SerializeField] private Animator animator;
     [SerializeField] [Range(0, 30)] private float animateSensitivity = 3;
-    [SerializeField] private string xAnimatorParameter = "X";
-    [SerializeField] private string yAnimatorParameter = "Y";
 
     private int rightStateIndex = Animator.StringToHash("right");
     private int leftStateIndex = Animator.StringToHash("left");
@@ -50,29 +48,6 @@ public class Player : MonoBehaviour
             }
         }
     }
-
-    /*
-    private void Animate(Vector3 _targetPosition)
-    {
-        Vector2 _delta = _targetPosition - transform.position;
-        Vector2 _ratio = VectorHelper.Divide(_delta, (Vector2)LookPositionOnPlane.Instance.Size);
-
-        int _x = 0;
-        if (Mathf.Abs(_ratio.x) > animateThreshold)
-        {
-            _x = RoundingHelper.InvertOnNegativeCeil(_ratio.x);
-        }
-
-        int _y = 0;
-        if (Mathf.Abs(_ratio.y) > animateThreshold)
-        {
-            _y = RoundingHelper.InvertOnNegativeCeil(_ratio.y);
-        }
-
-        animator.SetInteger(xAnimatorParameter, _x);
-        animator.SetInteger(yAnimatorParameter, _y);
-    }
-    */
 
     private void OnEnable()
     {
