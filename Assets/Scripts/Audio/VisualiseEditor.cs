307a6b534965cec7e88d347c6e6d4b9317fb4397
tommtom9 <tommtom9@live.nl>
2018-05-24 09:00:09 +0200

    Visual script onw doesnt cause a build to chrash

--- Assets/Scripts/Audio/VisualiseEditor.cs
+++ Assets/Scripts/Audio/VisualiseEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using System;
 
@@ -39,3 +40,4 @@ public class VisualiseEditor : Editor
         }
     }   
 }
+#endif
