f8b931cf11af4a82d3ccd85524018c5eef10c63d
Daniel Brand <idaniel95@me.com>
2018-05-24 06:55:32 +0200

    fixed colors not being detected in texture due to unity filtering

--- Assets/Scripts/Utilities/Helpers/TextureHelper.cs
+++ Assets/Scripts/Utilities/Helpers/TextureHelper.cs
@@ -19,15 +19,17 @@ public static class TextureHelper {
         return indexes;
     }
 
-    public static List<Vector2Int> GetPixelCoordinatesWithColor(this Texture2D texture, Color targetColor) {
+    public static List<Vector2Int> GetPixelCoordinatesWithColor(this Texture2D texture, Color minColor, Color maxColor) {
         int width = texture.width;
         int height = texture.height;
         Color[] colors = texture.GetPixels();
 
+        Debug.Log(colors.Length);
+
         List<Vector2Int> pixelCoordinates = new List<Vector2Int>();
 
         for (int i = 0; i < colors.Length; i++) {
-            if (colors[i] == targetColor) {
+            if (colors[i].WithinColorRange(minColor, maxColor)) {
                 Vector2Int pixelCoordinate = new Vector2Int();
                 pixelCoordinate.x = i % width;
                 pixelCoordinate.y = (i - pixelCoordinate.x) / width;
@@ -39,20 +41,18 @@ public static class TextureHelper {
         return pixelCoordinates;
     }
 
-    public static List<Vector3> GetWorldPositionsOfColor(this SpriteRenderer spriteRenderer, Color targetColor) {
+    public static List<Vector3> GetWorldPositionsOfColor(this SpriteRenderer spriteRenderer, Color minColor, Color maxColor) {
         Transform transform = spriteRenderer.transform;
         Texture2D texture = spriteRenderer.sprite.texture;
 
-        List<Vector2Int> pixelCoordinates = texture.GetPixelCoordinatesWithColor(targetColor);
+        List<Vector2Int> pixelCoordinates = texture.GetPixelCoordinatesWithColor(minColor, maxColor);
 
         List<Vector3> worldPositions = new List<Vector3>();
         float pixelsPerUnit = spriteRenderer.sprite.pixelsPerUnit;
 
         foreach (Vector2Int pixelCoordinate in pixelCoordinates) {
             Vector3 localPosition = (Vector2)pixelCoordinate / pixelsPerUnit;
-            //Debug.Log("localPosition = " + localPosition);
             Vector3 worldPosition = transform.TransformPoint(localPosition);
-            //Debug.Log("worldPosition = " + worldPosition);
 
             worldPositions.Add(worldPosition);
         }
@@ -67,6 +67,16 @@ public static class TextureHelper {
         Vector3 worldPosition = spriteRenderer.transform.TransformPoint(localPosition);
 
         return worldPosition;
+    }
+
+    private static bool WithinColorRange(this Color color, Color minColor, Color maxColor)
+    {
+        bool redMatch = color.r >= Mathf.Min(minColor.r, maxColor.r) && color.r <= Mathf.Max(minColor.r, maxColor.r);
+        bool greenMatch = color.g >= Mathf.Min(minColor.g, maxColor.g) && color.g <= Mathf.Max(minColor.g, maxColor.g);
+        bool blueMatch = color.b >= Mathf.Min(minColor.b, maxColor.b) && color.b <= Mathf.Max(minColor.b, maxColor.b);
+        bool alphaMatch = color.a >= Mathf.Min(minColor.a, maxColor.a) && color.a <= Mathf.Max(minColor.a, maxColor.a);
+
+        return redMatch && greenMatch && blueMatch && alphaMatch;
     }
 
 }
