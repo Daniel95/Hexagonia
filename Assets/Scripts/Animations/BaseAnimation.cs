9882fbf149b875d1beabfc10037b6a4b7076fd9c
Daniel Brand <idaniel95@me.com>
2018-05-25 14:37:30 +0200

    started working on fade animation (wip)

---
+++ Assets/Scripts/Animations/BaseAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class BaseAnimation : MonoBehaviour {
+
+    [SerializeField] [Range(0, 1)] protected float Speed;
+
+    private Action AnimationCompleted;
+
+    public void StartAnimation(Action _animationCompleted)
+    {
+        //_animationCompleted = 
+    }
+
+    public void StopAnimation()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Animations/FadeAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class FadeAnimation : IAnimation
+{
+
+    public void StartAnimation(Action onAnimationCompleted = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void StopAnimation()
+    {
+        throw new NotImplementedException();
+    }
+
+}
