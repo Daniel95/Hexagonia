87fa090a0e98a140c346bc79b1b436361ec9cc6c
tommtom9 <tommtom9@live.nl>
2018-05-31 12:08:57 +0200

    Expirimenting with a beat detection methode

---
+++ Assets/Scripts/Audio/AudioTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+
+    private void Start()
+    {
+        float[] _audioData = new float[/*audioSource.clip.samples * audioSource.clip.channels*/ 1000];
+
+        audioSource.clip.GetData(_audioData, 0);
+
+        for (int i = 0; i < _audioData.Length; i++)
+        {
+            //Debug.Log(_audioData[i]);
+        }
+    }
+}
