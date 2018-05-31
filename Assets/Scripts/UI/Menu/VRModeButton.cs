c467648165d65477d9bc9ebdb0f87621d2d1ebb8
Daniel Brand <idaniel95@me.com>
2018-05-31 15:18:35 +0200

    fixed saving loop

--- Assets/Scripts/UI/Menu/VRModeButton.cs
+++ Assets/Scripts/UI/Menu/VRModeButton.cs
@@ -1,4 +1,86 @@
-﻿using System;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// Button that calls the VRSwitch Switch method to switch VR Mode.
+/// </summary>
+[RequireComponent(typeof(Button))]
+public class VRModeButton : GazeButton
+{
+    private const string EVENT_SYSTEM_NAME = "EventSystem";
+
+    [SerializeField] private Color vrColor = Color.white;
+    [SerializeField] private Color nonVRColor = Color.yellow;
+
+    private EventSystem[] eventSystems;
+
+    /// <summary>
+    /// Called when the button is activated.
+    /// </summary>
+    public void OnClick()
+    {
+        bool _vrState = VRSwitch.Instance.Switch();
+
+        if (_vrState)
+        {
+            for (int i = 0; i < eventSystems.Length; i++)
+            {
+                if (eventSystems[i].transform.name == EVENT_SYSTEM_NAME)
+                {
+                    eventSystems[i].gameObject.SetActive(false);
+                }
+            }
+        }
+        else if (!_vrState)
+        {
+            for (int i = 0; i < eventSystems.Length; i++)
+            {
+                if (eventSystems[i].transform.name == EVENT_SYSTEM_NAME)
+                {
+                    eventSystems[i].gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void UpdateColor()
+    {
+        if(VRSwitch.VRState)
+        {
+            buttonImage.color = vrColor;
+        }
+        else
+        {
+            buttonImage.color = nonVRColor;
+        }
+    }
+
+    protected override void OnGazeFilled()
+    {
+        OnClick();
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        VRSwitch.SwitchedEvent += UpdateColor;
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        VRSwitch.SwitchedEvent -= UpdateColor;
+    }
+
+    private void Start()
+    {
+        eventSystems = Resources.FindObjectsOfTypeAll<EventSystem>();
+    }
+}
+
+/*
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -73,90 +155,6 @@ public class VRModeButton : GazeButton
         {
             InitializedEvent();
         }
-    }
-}
-
-/*
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
-
-/// <summary>
-/// Button that calls the VRSwitch Switch method to switch VR Mode.
-/// </summary>
-[RequireComponent(typeof(Button))]
-public class VRModeButton : GazeButton
-{
-    private const string EVENT_SYSTEM_NAME = "EventSystem";
-
-    [SerializeField] private Color vrColor = Color.white;
-    [SerializeField] private Color nonVRColor = Color.yellow;
-
-    private EventSystem[] eventSystems;
-
-    /// <summary>
-    /// Called when the button is activated.
-    /// </summary>
-    public void OnClick()
-    {
-        bool _vrState = VRSwitch.Instance.Switch();
-
-        if (_vrState)
-        {
-            for (int i = 0; i < eventSystems.Length; i++)
-            {
-                if (eventSystems[i].transform.name == EVENT_SYSTEM_NAME)
-                {
-                    eventSystems[i].gameObject.SetActive(false);
-                }
-            }
-        }
-        else if (!_vrState)
-        {
-            for (int i = 0; i < eventSystems.Length; i++)
-            {
-                if (eventSystems[i].transform.name == EVENT_SYSTEM_NAME)
-                {
-                    eventSystems[i].gameObject.SetActive(true);
-                }
-            }
-        }
-    }
-
-    private void UpdateColor()
-    {
-        if(VRSwitch.VRState)
-        {
-            buttonImage.color = vrColor;
-        }
-        else
-        {
-            buttonImage.color = nonVRColor;
-        }
-    }
-
-    protected override void OnGazeFilled()
-    {
-        OnClick();
-    }
-
-    protected override void OnEnable()
-    {
-        base.OnEnable();
-        VRSwitch.SwitchedEvent += OnClick;
-        VRSwitch.SwitchedEvent += UpdateColor;
-    }
-
-    protected override void OnDisable()
-    {
-        base.OnDisable();
-        VRSwitch.SwitchedEvent -= OnClick;
-        VRSwitch.SwitchedEvent -= UpdateColor;
-    }
-
-    private void Start()
-    {
-        eventSystems = Resources.FindObjectsOfTypeAll<EventSystem>();
     }
 }
 */
--- Assets/Scripts/VRSwitch/VRSwitch.cs
+++ Assets/Scripts/VRSwitch/VRSwitch.cs
@@ -1,4 +1,107 @@
-﻿using System;
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.XR;
+
+/// <summary>
+/// Contains current state of VR mode, and has public methods to switch VR mode.
+/// </summary>
+public class VRSwitch : MonoBehaviour
+{
+    public static VRSwitch Instance { get { return GetInstance(); } }
+    public static bool VRState { get { return vrState; } }
+
+    public static Action SwitchedEvent;
+
+    private static bool vrState;
+
+    #region Singleton
+    private static VRSwitch instance;
+
+    private static VRSwitch GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = FindObjectOfType<VRSwitch>();
+        }
+        return instance;
+    }
+    #endregion
+
+    private const string VR_MODE = "VRMode";
+
+    public GameObject GVRReticlePointerGameObject { get { return gvrReticlePointerGameObject; } }
+
+    [SerializeField] private GameObject gvrGameObject;
+
+    private GameObject gvrReticlePointerGameObject;
+
+    /// <summary>
+    /// Switches the VR Mode, returns the VR State.
+    /// </summary>
+    /// <returns></returns>
+    public bool Switch()
+    {
+        vrState = !vrState;
+        XRSettings.enabled = vrState;
+
+        PlayerPrefs.SetInt(VR_MODE, Convert.ToInt32(vrState));
+        PlayerPrefs.Save();
+
+        gvrGameObject.SetActive(vrState);
+        gvrReticlePointerGameObject.SetActive(vrState);
+#if !UNITY_EDITOR
+        if (vrState)
+        {
+            GvrCardboardHelpers.Recenter();
+        }
+#endif
+
+        if(SwitchedEvent != null)
+        {
+            SwitchedEvent();
+        }
+
+        return vrState;
+    }
+
+    private void Awake()
+    {
+        gvrReticlePointerGameObject = FindObjectOfType<GvrReticlePointer>().gameObject;
+
+        CoroutineHelper.DelayFrames(1, () => {
+            vrState = Convert.ToBoolean(PlayerPrefs.GetInt(VR_MODE));
+            XRSettings.enabled = vrState;
+            gvrGameObject.SetActive(vrState);
+
+            if (SwitchedEvent != null)
+            {
+                SwitchedEvent();
+            }
+        });
+    }
+
+    private void SetReticlePointer()
+    {
+        if (vrState)
+        {
+            gvrReticlePointerGameObject.SetActive(true);
+        }
+    }
+
+    private void OnEnable()
+    {
+        Player.DiedEvent += SetReticlePointer;
+    }
+
+    private void OnDisable()
+    {
+        Player.DiedEvent -= SetReticlePointer;
+    }
+}
+
+/*
+using System;
 using UnityEngine;
 using UnityEngine.XR;
 
@@ -94,103 +197,6 @@ public class VRSwitch : MonoBehaviour
     private void OnDisable()
     {
         VRModeButton.InitializedEvent -= SetVRModeAfterInitialization;
-        Player.DiedEvent -= SetReticlePointer;
-    }
-}
-
-/*
-using System;
-using UnityEngine;
-using UnityEngine.XR;
-
-/// <summary>
-/// Contains current state of VR mode, and has public methods to switch VR mode.
-/// </summary>
-public class VRSwitch : MonoBehaviour
-{
-    public static VRSwitch Instance { get { return GetInstance(); } }
-    public static bool VRState { get { return vrState; } }
-
-    public static Action SwitchedEvent;
-
-    private static bool vrState;
-
-    #region Singleton
-    private static VRSwitch instance;
-
-    private static VRSwitch GetInstance()
-    {
-        if (instance == null)
-        {
-            instance = FindObjectOfType<VRSwitch>();
-        }
-        return instance;
-    }
-    #endregion
-
-    private const string VR_MODE = "VRMode";
-
-    public GameObject GVRReticlePointerGameObject { get { return gvrReticlePointerGameObject; } }
-
-    [SerializeField] private GameObject gvrGameObject;
-
-    private GameObject gvrReticlePointerGameObject;
-
-    /// <summary>
-    /// Switches the VR Mode, returns the VR State.
-    /// </summary>
-    /// <returns></returns>
-    public bool Switch()
-    {
-        vrState = !vrState;
-        XRSettings.enabled = vrState;
-
-        PlayerPrefs.SetInt(VR_MODE, Convert.ToInt32(vrState));
-        PlayerPrefs.Save();
-
-        gvrGameObject.SetActive(vrState);
-        gvrReticlePointerGameObject.SetActive(vrState);
-#if !UNITY_EDITOR
-        if (vrState)
-        {
-            GvrCardboardHelpers.Recenter();
-        }
-#endif
-
-        return vrState;
-    }
-
-    private void Awake()
-    {
-        gvrReticlePointerGameObject = FindObjectOfType<GvrReticlePointer>().gameObject;
-
-        CoroutineHelper.DelayFrames(1, () => {
-            vrState = Convert.ToBoolean(PlayerPrefs.GetInt(VR_MODE));
-            XRSettings.enabled = vrState;
-            gvrGameObject.SetActive(vrState);
-
-            if (SwitchedEvent != null)
-            {
-                SwitchedEvent();
-            }
-        });
-    }
-
-    private void SetReticlePointer()
-    {
-        if (vrState)
-        {
-            gvrReticlePointerGameObject.SetActive(true);
-        }
-    }
-
-    private void OnEnable()
-    {
-        Player.DiedEvent += SetReticlePointer;
-    }
-
-    private void OnDisable()
-    {
         Player.DiedEvent -= SetReticlePointer;
     }
 }
