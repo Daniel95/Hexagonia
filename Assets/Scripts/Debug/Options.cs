d8a5a51346b30f5d97531909c3fcc9661522e0bd
Daniel Brand <idaniel95@me.com>
2018-06-04 12:47:35 +0200

    added debug options to go back to menu

--- Assets/Scripts/Debug/Options.cs
+++ Assets/Scripts/Debug/Options.cs
@@ -6,6 +6,13 @@ public partial class SROptions
 {
     [Category("Debug")] [Sort(-1)] public bool FPSCounterEnabled { get { return DebugLibrary.FPSCounterEnabled; } set { DebugLibrary.FPSCounterEnabled = value; } }
 
+    [Category("Debug")]
+    [Sort(-1)]
+    public void GoToMenuScene()
+    {
+        SceneLoader.Instance.SwitchScene(Scenes.Menu);
+    }
+
     [Category("VR Controls")]
     [Sort(-1)]
     public void UseDefaultVRControls()
