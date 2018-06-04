b49488db222ba232250a50476d21eeee0ae94258
Pim Horeman <pimhoreman@gmail.com>
2018-06-04 11:54:16 +0200

    -Added Cutscene,mp4 -Added CameraHolderIntro -Updated BuildSettings

---
+++ Assets/Scripts/Camera/CameraHolderIntro.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class CameraHolderIntro : CameraHolder
+{
+	[SerializeField] private VideoPlayer videoPlayer;
+	[SerializeField] private Scenes scene;
+
+
+	protected override void EnterScene()
+	{
+		videoPlayer.targetCamera = Camera.main;
+		videoPlayer.Play();
+		videoPlayer.loopPointReached += CheckVideoPlayer;
+		base.EnterScene();
+	}
+
+	private void CheckVideoPlayer(VideoPlayer vp)
+	{ 
+		SceneLoader.Instance.SwitchScene(scene);
+		videoPlayer.loopPointReached -= CheckVideoPlayer;
+	}
+
+	protected override void ExitScene()
+	{
+		base.ExitScene();
+	}
+}
