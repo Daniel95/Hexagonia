b1817aa5a1a6c58dae11d5c13a8d3d71df91709a
DANNYPC\danny <dannykruiswijk@hotmail.com>
2018-06-06 10:03:23 +0200

    Added FOV changer in the intro

--- Assets/Scripts/Camera/CameraHolderIntro.cs
+++ Assets/Scripts/Camera/CameraHolderIntro.cs
@@ -3,21 +3,31 @@ using UnityEngine.Video;
 
 public class CameraHolderIntro : CameraHolder
 {
-	[SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] [Range(0, 90)] private float nonVRFOV = 60;
+
+    [SerializeField] private VideoPlayer videoPlayer;
 	[SerializeField] private Scenes scene;
 	private GameObject gvrPointer;
 
-	protected override void EnterScene()
-	{
-		//gvrPointer = GameObject.Find("GvrReticlePointer");
-	    gvrPointer = Resources.FindObjectsOfTypeAll<GvrReticlePointer>()[0].transform.gameObject;
+    private float previousFOV;
 
+    protected override void EnterScene()
+	{
+	    base.EnterScene();
+
+        gvrPointer = Resources.FindObjectsOfTypeAll<GvrReticlePointer>()[0].transform.gameObject;
         gvrPointer.SetActive(false);
+
 		videoPlayer.targetCamera = Camera.main;
 		videoPlayer.Play();
 		videoPlayer.loopPointReached += CheckVideoPlayer;
-		base.EnterScene();
-	}
+
+	    if (!VRSwitch.VRState)
+	    {
+	        previousFOV = Camera.main.fieldOfView;
+	        Camera.main.fieldOfView = nonVRFOV;
+	    }
+    }
 
 	private void CheckVideoPlayer(VideoPlayer vp)
 	{ 
@@ -28,5 +38,10 @@ public class CameraHolderIntro : CameraHolder
 	protected override void ExitScene()
 	{
 		base.ExitScene();
-	}
+
+	    if (!VRSwitch.VRState)
+	    {
+	        Camera.main.fieldOfView = previousFOV;
+	    }
+    }
 }
