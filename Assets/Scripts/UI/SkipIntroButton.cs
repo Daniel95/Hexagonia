aa6f74b86dd80d8c190824bc20f5f9f05ac35b38
DANNYPC\danny <dannykruiswijk@hotmail.com>
2018-06-06 17:28:34 +0200

    Fixed errors

--- Assets/Scripts/UI/SkipIntroButton.cs
+++ Assets/Scripts/UI/SkipIntroButton.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 /// <summary>
 /// Button that skips the Intro cutscene when pressed
@@ -7,21 +8,33 @@ public class SkipIntroButton : GazeButton {
 
     [SerializeField] private Scenes scene;
 
+    [SerializeField] private const string NOT_FIRST_TIME_LAUNCHED = "FirstTimeLaunched";
+
     protected override void OnTrigger()
     {
-        SceneLoader.Instance.SwitchScene(scene);
+        if (Convert.ToBoolean(PlayerPrefs.GetInt(NOT_FIRST_TIME_LAUNCHED)))
+        {
+            SceneLoader.Instance.SwitchScene(scene);
+        }
     }
 
     protected override void OnEnable()
     {
         base.OnEnable();
         PlatformBaseInput.DownInputEvent += OnInput;
+        CameraHolderIntro.OnCompletedIntroVideoEvent += SetFirstTimeLaunchedPrefs;
     }
 
     protected override void OnDisable()
     {
         base.OnDisable();
         PlatformBaseInput.DownInputEvent -= OnInput;
+        CameraHolderIntro.OnCompletedIntroVideoEvent -= SetFirstTimeLaunchedPrefs;
+    }
+
+    private void SetFirstTimeLaunchedPrefs()
+    {
+        PlayerPrefs.SetInt(NOT_FIRST_TIME_LAUNCHED, Convert.ToInt32(true));
     }
 
     private void OnInput(Vector2 _position)
