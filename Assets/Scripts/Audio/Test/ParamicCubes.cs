08e67d357ae7a87ff9b9bdd16355f69d2d51af9d
Pim Horeman <pimhoreman@gmail.com>
2018-06-07 08:42:40 +0200

    -Test Material -Added ParamicCubes.cs << can propably be removed after testing -Addded AudioPeer_test.cs << can propably be removed after testing

---
+++ Assets/Scripts/Audio/Test/ParamicCubes.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ParamicCubes : MonoBehaviour {
+
+	[SerializeField] private int band;
+	[SerializeField] private float startScale, maxScale;
+	[SerializeField] private bool useBuffer;
+	
+	void Update () {
+		if(useBuffer)
+		{
+			transform.localScale = new Vector3(transform.localScale.x, (AudioPeer_test.audioBandBuffer[band] * maxScale) + startScale, transform.localScale.z);
+		}
+		if(!useBuffer)
+		{
+			transform.localScale = new Vector3(transform.localScale.x, (AudioPeer_test.audioBand[band] * maxScale) + startScale, transform.localScale.z);
+		}
+	}
+}
