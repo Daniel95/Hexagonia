baee1744bdd15b62c63d192cf579d0923bfbf39f
tommtom9 <tommtom9@live.nl>
2018-06-07 13:36:44 +0200

    Disabled engine sound for now

--- Assets/Scripts/Audio/PlayerEngineSound.cs
+++ Assets/Scripts/Audio/PlayerEngineSound.cs
@@ -11,13 +11,25 @@ public class PlayerEngineSound : MonoBehaviour {
     [SerializeField] float minSpeed;
     [SerializeField] float maxSpeed;
 
+    [Space(5)]
+
+    [SerializeField] float buffer = 0.1f;
+
     [SerializeField] AudioSource source;
 
     void Update ()
     {
         float _delta = SmoothPlaneMovement.Delta.sqrMagnitude;
-        
-        source.volume = (maxVolume * Mathf.InverseLerp(minSpeed, maxSpeed, _delta));
+        float _volumeDelta = (maxVolume * Mathf.InverseLerp(minSpeed, maxSpeed, _delta));
+
+        if (_volumeDelta >= source.volume)
+        {
+            source.volume = (maxVolume * Mathf.InverseLerp(minSpeed, maxSpeed, _delta));
+        }
+        else
+        {
+            source.volume -= buffer * Time.deltaTime;
+        }
 
         //Debug.Log(Mathf.InverseLerp(minSpeed, maxSpeed, _delta));
     }
