df84960385d308b69d10dac748c8c2a1c82eff87
Daniel Brand <idaniel95@me.com>
2018-06-09 01:50:23 +0200

    removed comments in playercollision

--- Assets/Scripts/Player/PlayerCollisions.cs
+++ Assets/Scripts/Player/PlayerCollisions.cs
@@ -19,51 +19,17 @@ public class PlayerCollisions : MonoBehaviour
     private void Update()
     {
         UpdateColliders();
-        //UpdateRaycastCollisions();
+        UpdateRaycastCollisions();
     }
 
     private void UpdateColliders()
     {
-        //DebugHelper.SetDebugPosition(PlayerTrails.Instance.TopLeftPosition, "TopLeftPosition", false, Color.green);
-        //DebugHelper.SetDebugPosition(PlayerTrails.Instance.TopRightWingPosition, "TopRightWingPosition", false, Color.blue);
-        //DebugHelper.SetDebugPosition(PlayerTrails.Instance.BottomLeftPosition, "BottomLeftPosition", false, Color.yellow);
-        //DebugHelper.SetDebugPosition(PlayerTrails.Instance.BottomRightPosition, "BottomRightPosition", false, Color.red);
-
-        //float _topLeftToBottomRightAngle = Vector3.Angle(PlayerTrails.Instance.TopLeftPosition, PlayerTrails.Instance.BottomRightPosition);
-        //float _topRightToBottomLeftAngle = Vector3.Angle(PlayerTrails.Instance.TopRightWingPosition, PlayerTrails.Instance.BottomLeftPosition);
-
         topLeftToBottomRightCollider.transform.position = (PlayerTrails.Instance.TopLeftPosition + PlayerTrails.Instance.BottomRightPosition) / 2f;
         topLeftToBottomRightCollider.transform.rotation = Quaternion.LookRotation(PlayerTrails.Instance.TopLeftPosition - PlayerTrails.Instance.BottomRightPosition, Vector3.forward);
 
         topRightToBottomLeftCollider.transform.position = (PlayerTrails.Instance.TopRightWingPosition + PlayerTrails.Instance.BottomLeftPosition) / 2f;
         topRightToBottomLeftCollider.transform.rotation = Quaternion.LookRotation(PlayerTrails.Instance.TopRightWingPosition - PlayerTrails.Instance.BottomLeftPosition, Vector3.forward);
     }
-
-    //private void UpdateColliders()
-    //{
-    //    DebugHelper.SetDebugPosition(PlayerTrails.Instance.TopLeftPosition, "TopLeftPosition", false, Color.green);
-    //    DebugHelper.SetDebugPosition(PlayerTrails.Instance.TopRightWingPosition, "TopRightWingPosition", false, Color.blue);
-    //    DebugHelper.SetDebugPosition(PlayerTrails.Instance.BottomLeftPosition, "BottomLeftPosition", false, Color.yellow);
-    //    DebugHelper.SetDebugPosition(PlayerTrails.Instance.BottomRightPosition, "BottomRightPosition", false, Color.red);
-
-    //    float _topLeftToBottomRightAngle = Vector3.Angle(PlayerTrails.Instance.TopLeftPosition, PlayerTrails.Instance.BottomRightPosition);
-    //    float _topRightToBottomLeftAngle = Vector3.Angle(PlayerTrails.Instance.TopRightWingPosition, PlayerTrails.Instance.BottomLeftPosition);
-
-    //    Vector3 _topLeftToBottomRightColliderEulerAngles = topLeftToBottomRightCollider.transform.eulerAngles;
-    //    Vector3 _topRightToBottomLeftColliderEulerAngles = topRightToBottomLeftCollider.transform.eulerAngles;
-
-    //    topLeftToBottomRightCollider.transform.localRotation = Quaternion.Euler(new Vector3(_topLeftToBottomRightColliderEulerAngles.x, _topLeftToBottomRightColliderEulerAngles.y, _topLeftToBottomRightAngle));
-    //    topRightToBottomLeftCollider.transform.localRotation = Quaternion.Euler(new Vector3(_topRightToBottomLeftColliderEulerAngles.x, _topRightToBottomLeftColliderEulerAngles.y, -_topRightToBottomLeftAngle));
-    //}
-
-    //private static void SetPos(Transform _transform, Vector3 _start, Vector3 _end)
-    //{
-    //    Vector3 dir = _end - _start;
-    //    Vector3 mid = (dir) / 2f + _start;
-    //    _transform.position = mid;
-    //    _transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);
-    //    //transform.localScale.y = new Vector3(transform.localScale.x, dir.magnitude, transform.localScale.z);
-    //}
 
     private void UpdateRaycastCollisions()
     {
